90e2a82c5facf288817c34f5cea13b791d3f85f3
satonari205 <tomoyahoshi205@outlook.jp>
2023-09-19 21:46:55 +0900

    20230919

--- Django/config/bbs/urls.py
+++ Django/config/bbs/urls.py
@@ -5,6 +5,7 @@ app_name = 'bbs'
 
 urlpatterns = [
     path('tweets/', views.TweetListAPIView.as_view(), name="tweets"),
+    path('tweets/create', views.TweetCreateAPIView.as_view(), name="tweets"),
     path('replies/', views.TweetAndRepliesAPIView.as_view(), name="replies"),
     # path('replies/', views.ReplyListAPIView.as_view(), name="replies"),
 ]
--- Django/config/bbs/views.py
+++ Django/config/bbs/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
-
-from rest_framework import generics
-from drf_multiple_model.views import ObjectMultipleModelAPIView
-
 from .models import Tweet,Reply
 from .serializers import TweetSerializer,ReplySerializer
 
+from rest_framework.generics import ListAPIView,CreateAPIView
+from drf_multiple_model.views import ObjectMultipleModelAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-class TweetListAPIView(generics.ListAPIView):
+class TweetListAPIView(ListAPIView):
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
 
@@ -30,37 +27,5 @@ class TweetAndRepliesAPIView(APIView):
         }
         return Response(data, status=status.HTTP_200_OK)
 
-# class TweetAPIViews(generics.RetrieveAPIView):
-#     serializer_class = TweetSerializer
-
-#     def get_queryset(self):
-#         queryset = Tweet.objects.all()
-#         tweet_id = self.request.query_params.get('id')
-#         if tweet_id is not None:
-#             queryset = queryset.filter(id = tweet_id)
-#         return queryset
-
-# class ReplyListAPIView(generics.ListAPIView):
-#     serializer_class = ReplySerializer
-
-#     def get_queryset(self):
-#         # queryset_tweet = Tweet.objects.all()
-#         queryset = Reply.objects.all()
-#         tweet_id = self.request.query_params.get('tweet',None)
-#         # tweet_id = self.request.query_params.get('tweet')
-#         if tweet_id is not None:
-#             # queryset_tweet = queryset_tweet.filter(id = tweet_id)
-#             queryset = queryset.filter(tweet = tweet_id)
-#         return queryset
-
-# class ReplyListAPIView(ObjectMultipleModelAPIView):
-
-#     def get_queryset(self,request):
-#         tweet_id = request.query_params.get('tweet')
-#         querylist = (
-#             {'queryset': Tweet.objects.filter(id = tweet_id),
-#             'serializer_class': TweetSerializer},
-#             {'queryset': Reply.objects.filter(tweet= tweet_id),
-#             'serializer_class': ReplySerializer},
-#         )
-#         return querylist
+class TweetCreateAPIView(CreateAPIView):
+    serializer_class = TweetSerializer
