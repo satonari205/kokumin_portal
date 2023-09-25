5418f81c7bca1887084eaab9abba3eed6ba70f72
satonari205 <tomoyahoshi205@outlook.jp>
2023-09-25 21:47:26 +0900

    20230925

--- React/portal-front/src/Navigation/CurrentUser.jsx
+++ React/portal-front/src/Navigation/CurrentUser.jsx
@@ -1,19 +1,25 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect,useContext } from "react";
 import { Link } from "react-router-dom";
 import Logout from "./Logout";
+import { UserContext } from "../context/userContext";
 
 const CurrentUser = () => {
-    const [user,setUser] = useState();
+  const { user,setUser } = useContext(UserContext);
 
-    const baseURL = 'http://127.0.0.1:8000/api/';
+  const baseURL = 'http://127.0.0.1:8000/api/';
 
     useEffect(()=>{
         async function currentUser () {
-            await axios.get(baseURL + 'users/current/')
+            await axios.get(baseURL + 'users/current/',{
+				headers:{
+					'Content-Type': 'application/json',
+				},
+				withCredentials: true,
+            })
             .then(res => {
                 console.log(res.data);
-                setUser(res.data)
+                setUser(res.data);
             })
             .catch(error => {
                 console.log(error);
@@ -23,36 +29,37 @@ const CurrentUser = () => {
     },[])
 
     return(
-        <>
-            {user && (
-        <li>
-          <Link to={`users/${user.id}`}>
-            <span className="link link-warning">
-                {user.nickname} 
-            </span>
-            さん
-          </Link>
-        </li>
-      )}
-      {user ? (
-        <li>
-          <Logout />
-        </li>
-      ) : (
-        <>
-          <li>
-            <Link to={"/login"}>
-              ログイン
-            </Link>
-          </li>
-          <li>
-            <Link to={"/register"}>
-              ユーザー登録
-            </Link>
-          </li>
-        </>
-      )}
-        </>
+      <>
+        {user
+          ?
+            <>
+              <li>
+                <Link to={`users/${user.id}`}>
+                  <span className="link link-warning">
+                    {user.nickname}
+                  </span>
+                  さん
+                </Link>
+              </li>
+              <li>
+                <Logout setUser={setUser}/>
+              </li>
+            </>
+          :
+            <>
+              <li>
+                <Link to={"/login"}>
+                  ログイン
+                </Link>
+              </li>
+              <li>
+                <Link to={"/register"}>
+                  ユーザー登録
+                </Link>
+              </li>
+            </>
+          }
+      </>
     );
 }
 
--- React/portal-front/src/context/userContext.jsx
+++ React/portal-front/src/context/userContext.jsx
@@ -1,16 +1,13 @@
-// import { createContext, useState } from "react";
+import { createContext, useState } from "react";
 
-// export const UserContext = createContext();
+export const UserContext = createContext();
 
-// export const UserProvider = ({children}) => {
-//     const [user, setUser] = useState({
-//         "pk": null,
-//         "username": "ゲスト",
-//     },);
+export const UserProvider = ({children}) => {
+    const [user, setUser] = useState(null);
 
-//     return(
-//         <UserContext.Provider value={{user, setUser}}>
-//             {children}
-//         </UserContext.Provider>
-//     );
-// };
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    );
+};
